7c932a814ead0bfaea98dca65abd2f7024633a31
zhron <zhron@9bc5f1d9-0611-0410-9c6f-ee8cdef66cb5>
2009-08-03 21:55:49 +0000

    adding debug code to BootStrap class
    git-svn-id: http://paalpha.svn.broadbandmechanics.com/svn/core_v2/ecto_CM@5536 9bc5f1d9-0611-0410-9c6f-ee8cdef66cb5

--- web/includes/classes/BootStrap.class.php
+++ web/includes/classes/BootStrap.class.php
@@ -367,7 +367,7 @@ class BootStrap {
 echo "\$network_prefix = $network_prefix, \$network_folder = $network_folder" .
      "\$this->domain_prefix = $this->domain_prefix, \$this->domain_suffix = $this->domain_suffix" .
      "\$base_url_pa = $base_url_pa, \$base_url = $base_url";
-//die();
+die();
     // Finally - Load network!
     PA::$network_info = get_network_info(); // NOTE this should be retrieved from network XML config file
     PA::$extra = unserialize(PA::$network_info->extra);
