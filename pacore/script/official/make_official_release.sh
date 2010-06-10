9d7dcc68177fb70643f538a9c08dbf6d3ec2c4ff
Owen Bell <oab7@case.edu>
2010-06-10 18:19:24 -0400

    Moved scripts to tools

--- pacore/script/official/make_official_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# this script builds an official release from http://update.peopleaggregator.org/svn/release
-`dirname $0`/make_release.sh release
--- pacore/script/version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This file holds the current PeopleAggregator version.
-
-# Don't forget to update web/config/default_config.php too if bumping the version number,
-# and make a note in the log in web/update/version.php.
-
-PA_VERSION=2.1.5
