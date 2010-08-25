276c8836728b183d3a0cf4a22bb363c161389cbb
Martin Spernau <martin@traumwind.de>
2010-08-25 19:36:00 +0200

    * rats, forgot to check this all in and thus had to re-write it ^^

--- pacore/web/BlockModules/GroupBulletinsModule/GroupBulletinsModule.php
+++ pacore/web/BlockModules/GroupBulletinsModule/GroupBulletinsModule.php
@@ -23,7 +23,67 @@ class GroupBulletinsModule extends Module {
     $this->title = __("Send Group Bulletins");
     $this->html_block_id = "GroupBulletinsModule";
   }
+  
+  function initializeModule($request_method, $request_data) {
+		if (empty(request_data['gid'])) return 'skip'; // sanity check
+		
+		// we do this checl only if the user is not already permitted to manage ads
+		$gp_access = PermissionsHandler::can_group_user(PA::$login_uid, $request_data['gid'], array('permissions' => 'manage_groups'));
+		if (! $gp_access) return 'skip'; // user shoudn't have gotten here in the first place, just don't show anything
 
+			if ($request_method == 'POST') {
+				$value_to_validate = array('title'=>'Title','bulletin_body'=>'Bulletin body');
+				foreach ($value_to_validate as $key=>$value) {
+					$request_data[$key] = trim($request_data[$key]);
+					if (empty($request_data[$key])) {
+					$error_msg .= $value.' can not be empty<br>';
+					}
+				}
+				
+				if (!$error_msg) { // if no errors yet
+					$subject = $request_data['title'];
+					$bull_message = $request_data['bulletin_body'];
+					$group = new Group();
+					$group->load($request_data['gid']);
+					
+					if (!empty($request_data['bulletins'])) { // send to all members
+						if (PA::$network_info) { // get group members
+							$gms = $group->get_members();
+							foreach ($gms as $i=>$m) {
+								$u = new User();
+								$u->load((int)$m{'user_id']);
+								$to_members[] = $u;
+							} 
+						}
+					} else if (!empty($request_data['send_to_me_only'])) { // test send to admin user
+						$to_members = array(PA::$login_user);
+					}
+					$this->sent_to = array();
+					// send it
+					if (!empty($to_member)) {
+						foreach($to_member as $recipient) {
+							$this->sent_to[] = $recipient->display_name;
+							PANotify::send("group_bulletin_sent", 
+								$recipient, 
+								$group, 
+								array(
+									'bulletin.message' => $bull_message,
+									'bulletin.subject' => $subject
+								));
+						}
+					}
+					
+					// wannt a preview with that?
+					if (!empty($request_data['preview'])) { // if preview is selected.
+						$container_html = 'default_email_container.tpl';
+						$email_container = & new Template('config/email_containers/'.$container_html);
+						$email_container->set('subject', $subject);
+						$email_container->set('message', $bull_message);
+						$preview_msg = $email_container->fetch();
+					}
+				}
+			}  }
+  
    function render() {    
     $this->inner_HTML = $this->generate_inner_html();
     $content = parent::render();
@@ -31,18 +91,10 @@ class GroupBulletinsModule extends Module {
   }
 
   function generate_inner_html() {
-     
-    switch( $this->mode ) {
-      default:
-        $tmp_file = PA::$blockmodule_path .'/'. get_class($this) . '/center_inner_public.tpl';
-    }
+    $tmp_file = PA::$blockmodule_path .'/'. get_class($this) . '/center_inner_public.tpl';
     $group_bulletin = & new Template($tmp_file, $this);
-    $group_bulletin->set('config_navigation_url',
-                       network_config_navigation('bulletins'));
-    
     $group_bulletin->set('preview_msg', $this->preview_msg);
     $inner_html = $group_bulletin->fetch();
-    
     return $inner_html;
   }
 }
--- pacore/web/BlockModules/GroupBulletinsModule/center_inner_public.tpl
+++ pacore/web/BlockModules/GroupBulletinsModule/center_inner_public.tpl
@@ -4,7 +4,7 @@
 
 <div class="description"><?= __("Use this tool to send announcements to all registered network members") ?>. </div>
 <div id="div_epm1">
-  <form method="post" action="<?php echo PA::$url;?>/network_bulletins.php">
+  <form method="post" action="">
   <fieldset class="center_box">
   <div class="field">
     <h4><label><?= __("Title") ?>:</label></h4>
@@ -15,32 +15,7 @@
     <h4><label><?= __("Message/post") ?>:</label></h4>
      <textarea name="bulletin_body" cols="58" rows="10" class="text longer"><?php echo htmlspecialchars(@$_POST['bulletin_body']);?></textarea>
   </div>
-  
-  <div class="field">
-    <h4><label><?= __("Tags") ?>:</label></h4>
-     <input name="tags" type="text" class="text longer" size="60" value="<?php echo htmlspecialchars(@$_POST['tags']);?>"/>
-  </div>
-  
-  
-  <h4><label><?= __("Destination") ?>:</label></h4>
-    <?php $checked = '';if(!empty($_POST['inbox'])) { $checked = 'CHECKED';} else { $checked = ''; }?>
-    <div class="field">
-      <input type="checkbox" name="inbox" value="1" <?php echo $checked ;?> />
-      &nbsp;<?= __("Deliver message to all registered members by internal mail") ?>
-    </div>
-      
-    <?php $checked = '';if(!empty($_POST['mail'])) { $checked = 'CHECKED';} else { $checked = ''; }?>
-    <div class="field">
-      <input type="checkbox" name="mail" value="1" <?php echo $checked ;?> />
-      &nbsp;<?= __("Email message to all network members") ?>
-    </div>
-    <?php $checked = '';if(!empty($_POST['network_home'])) { $checked = 'CHECKED';} else {
-    $checked = ''; }?>
-    <div class="field">
-      <input type="checkbox" name="network_home" value="1" <?php echo $checked ;?>/>
-      &nbsp;<?= __("Post message to network home page community blog") ?> 
-    </div>
-    
+
     <div class="button_position"> 
       <input type="submit" name="preview" value="<?= __("Preview") ?>"/>
       <input type="submit" name="send_to_me_only" value="<?= __("Send to me only") ?>"/>
