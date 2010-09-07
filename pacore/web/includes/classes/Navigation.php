bb1002ddfc5a35144f999daee6fa7d0ac27f79b7
Brian Stack <bis12@case.edu>
2010-09-07 01:09:12 -0400

    Fix a permissions problem with the group poll.

--- pacore/web/includes/classes/Navigation.php
+++ pacore/web/includes/classes/Navigation.php
@@ -484,11 +484,6 @@ class Navigation {
     		'caption' => sprintf(__('%s Gallery'), __(PA::$group_noun)),
     		'url' => $this->base_url . PA_ROUTE_MEDIA_GALLEY_IMAGES . '/view=groups_media&amp;gid='.$group_id
 	),
-	'group_poll' => array(
-    		'caption' => sprintf(__('%s Poll'), __(PA::$group_noun)),
-    		'url' => $this->base_url .'/group_poll.php?gid='.$group_id
-    		),
-
     	'group_events' => array(
     		'caption' => sprintf(__('%s Events'), __(PA::$group_noun)),
     		'url' => $this->base_url.'/'.FILE_GROUP_CALENDAR.'?gid='.$group_id
@@ -550,7 +545,11 @@ class Navigation {
 						'settings' => array(
 							'caption' => __("Group Settings"),
 							'url' => PA::$url .'/addgroup.php?gid='.$gid
-						)
+						),
+						'group_poll' => array(
+					    		'caption' => sprintf(__('%s Poll'), __(PA::$group_noun)),
+    							'url' => $this->base_url .'/group_poll.php?gid='.$group_id
+				    		),
 					);
 				}
 				
